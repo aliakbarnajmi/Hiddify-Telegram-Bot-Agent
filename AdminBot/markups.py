8043e3107798037b15df413999ddba4628a78f42
Jelingam <89634871+Jelingam@users.noreply.github.com>
2025-04-23 23:26:10 +0330

    V1.1

--- AdminBot/markups.py
+++ AdminBot/markups.py
@@ -260,8 +260,7 @@ def users_bot_orders_management_markup(value=None):
 def confirm_payment_by_admin(payment_id):
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
-    markup.add(
-        InlineKeyboardButton(KEY_MARKUP['CONFIRM_PAYMENT'], callback_data=f"confirm_payment_by_admin:{payment_id}"))
+    markup.add(        InlineKeyboardButton(KEY_MARKUP['CONFIRM_PAYMENT'], callback_data=f"confirm_payment_by_admin:{payment_id}"))
     markup.add(InlineKeyboardButton(KEY_MARKUP['NO'], callback_data=f"cancel_payment_by_admin:{payment_id}"))
     markup.add(InlineKeyboardButton(KEY_MARKUP['SEND_MESSAGE'], callback_data=f"send_message_by_admin:{payment_id}"))
     return markup
@@ -269,10 +268,10 @@ def confirm_payment_by_admin(payment_id):
 def confirm_agent_registration_by_admin(telegram_id):
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
-    markup.add(
-        InlineKeyboardButton(KEY_MARKUP['CONFIRM_REGISTRATION'], callback_data=f"confirm_registration_by_admin:{telegram_id}"))
+    markup.add(InlineKeyboardButton(KEY_MARKUP['CONFIRM_REGISTRATION'], callback_data=f"confirm_registration_by_admin:{telegram_id}"))
     markup.add(InlineKeyboardButton(KEY_MARKUP['NO'], callback_data=f"cancel_registration_by_admin:{telegram_id}"))
     markup.add(InlineKeyboardButton(KEY_MARKUP['SEND_MESSAGE'], callback_data=f"send_message_by_admin:{telegram_id}"))
+    return markup
 
 def send_message_to_user_markup(admin_id):
     markup = InlineKeyboardMarkup()
